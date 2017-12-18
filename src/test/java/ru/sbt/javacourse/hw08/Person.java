89f97e188bd55152f0d27514635bc4880cc818f2
Alexey <a@a.ru>
2017-12-19 00:50:10 +0300

    Commit 02

--- src/test/java/ru/sbt/javacourse/hw08/Person.java
+++ src/test/java/ru/sbt/javacourse/hw08/Person.java
@@ -20,3 +20,49 @@ public class Person {
         return age;
     }
 }
+
+class Employee extends Person {
+    private String company;
+    private int salary;
+
+    public Employee(String name, int age, String company, int salary) {
+        super(name, age);
+        this.company = company;
+        this.salary = salary;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+}
+
+
+class Postman extends Employee {
+    private String region;
+
+    public Postman(String name, int age, String company, int salary, String region) {
+        super(name, age, company, salary);
+        this.region = region;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+}
+
+class Pochtaclerk extends Postman {
+    private String zip;
+
+    public Pochtaclerk(String name, int age, String company, int salary, String region, String zip) {
+        super(name, age, company, salary, region);
+        this.zip = zip;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+}
--- src/test/java/ru/sbt/javacourse/hw08/StreamsTest.java
+++ src/test/java/ru/sbt/javacourse/hw08/StreamsTest.java
@@ -6,19 +6,18 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Stream;
 
+import static org.junit.Assert.assertEquals;
+
 public class StreamsTest {
     @Test
     public void test01() throws Exception {
-
-        List<Integer> list = Arrays.asList(3, 5, 7, 9, 11);
-
+//        List<Integer> list = Arrays.asList(3, 5, 7, 9, 11);
         List<Person> persons = Arrays.asList(
                 new Person("Aaa", 11),
                 new Person("Bbb", 22),
                 new Person("Ccc", 33));
 
         Map<String, Person> map = null;
-
         map = Streams.of(persons)
                 .filter(p -> p.getAge() > 20)
                 .transform(p -> new Person("Mr. " + p.getName(), p.getAge() + 10))
@@ -28,7 +27,30 @@ public class StreamsTest {
             Person p = map.get(s);
             System.out.println(p.getName() + " " + p.getAge());
         }
+        assertEquals("Mr. Bbb", map.get("Mr. Bbb").getName());
+        assertEquals(32, map.get("Mr. Bbb").getAge());
+        assertEquals("Mr. Ccc", map.get("Mr. Ccc").getName());
+        assertEquals(43, map.get("Mr. Ccc").getAge());
+    }
 
+    @Test
+    public void test02() {
 
+        List<Postman> list = Arrays.asList(
+                new Postman("Aaa", 11, "Micro", 100, "New Moscow"),
+                new Postman("Bbb", 22, "Macro", 200, "Moscow"),
+                new Pochtaclerk("Ccc", 33, "Micro", 150, "Khimki", "14"));
+
+        Map<String, Person> map = null;
+//        Map<Integer, String> map = null;
+
+        Streams.of(list).filter(p -> p.getSalary() > 100);
+        map = Streams.of(list)
+                .transform(p -> new Employee(p.getName(), p.getAge(), p.getCompany(), p.getSalary()))
+                .toMap(e -> e.getName(), e -> e);
+
+//        Streams.of(list).transform(e -> e.getAge()).toMap(e -> e, e -> e.toString());
+//        map = Streams.of(list).transform(e -> e.getAge()).toMap(e -> e, e -> e.toString());
+        System.out.println(map.toString());
     }
 }
